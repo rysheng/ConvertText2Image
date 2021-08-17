101babb506d3e918e0ea6d7746f20f32409b1eec
Rysh <nghiathinh2000@gmail,com>
2021-08-17 16:45:36 +0700

    New Update

--- myscript.js
+++ myscript.js
@@ -1,5 +1,17 @@
-var list_text = [];
-var list_Canvas = [];
+var list_text = new Array();
+var list_Canvas =  new Array();
+var start_counter;
+
+
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
 
 
 // read text 
@@ -14,68 +26,67 @@ function read_text(){
         }
         fr.readAsText(this.files[0]);
     })
+    document.getElementById('second_row').innerHTML = "";
+    list_Canvas = new Array();
 }
 
 
 // preview image
 function preview_all_image(){
     list_text = document.getElementById('output').textContent.split('\n');
-    if(list_text){
-        for(let i=0; i < list_text.length; i++){
-            var pre = document.createElement("pre");
-            pre.textContent = list_text[i];
-            pre.id = String(i);
-            $('#second_row').append(pre);
-            
-        }
+    
+    for(let i=0; i < list_text.length; i++){
+        var pre = document.createElement("pre");
+        pre.textContent = list_text[i];
+        pre.id = i;
+        $('#second_row').append(pre);
     }
+
 
     for(let i=0; i < list_text.length; i++){
-        html2canvas(document.getElementById(i), {
-            onrendered: function (canvas) { 
-                list_Canvas[i] = canvas;
-            }
-        })
+        html2canvas(document.getElementById(i)).then(canvas => {
+            list_Canvas[i] = canvas;
+        });
     }
+
 }
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  }
+
 
 //download image 
 function down_all_image(){
-    var imgageData
-
-    if(!localStorage.length){
-        for(let i=0; i < list_Canvas.length; i++){
-            imgageData = list_Canvas[i].toDataURL("image/png");
-            localStorage.setItem(i, imgageData);
-        }
-    }
 
     var count_image = localStorage.length-1;
     while(count_image){
-        var recentImageURL = localStorage.getItem(localStorage.key(count_image))
+        var recentImageURL = localStorage.getItem(count_image)
 
         var a = $("<a>")
                 .attr("href", recentImageURL)
-                .attr("download", 'img' + localStorage.key(count_image) + '.png')
+                .attr("download", 'img' + count_image + '22222222222.png')
                 .appendTo("body");
         
         a[0].click();
         a.remove();
+        if(count_image==start_counter) break;
         count_image -=1;
-        sleep(500);
+        sleep(200);
+        
     }
 }
 
 function clear_localstorage(){
     localStorage.clear();
+    
 }
+
+function save_localstorage(){
+    var imgageData;
+    start_counter = localStorage.length;
+    for(let i=0; i < list_Canvas.length; i++){
+        imgageData = list_Canvas[i].toDataURL("image/png");
+        localStorage.setItem(String(i+start_counter), imgageData);
+    }
+    // console.log(start_counter, localStorage.length);
+}
+    
 
